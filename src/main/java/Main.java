6b04e10a758ca924952e93ffc8ce96fb72f8b034
Timofei <t.a.maksimenko@ya.ru>
2021-12-06 12:51:51 +0300

    fixed #1 add sout result

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
+    long result = service.calculate(amount, registered);
 
-    service.calculate(amount, registered);
+    System.out.println(result);
   }
+
 }
 
